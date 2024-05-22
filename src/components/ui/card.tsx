f7f0daecd9748c3ef3ead975041a3fee79fa44e7
Kaamraan <kraboobee@gmail.com>
2024-05-22 13:28:29 +0200

    fine tune card

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Card = React.forwardRef<
+const ShadCard = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
@@ -15,7 +15,7 @@ const Card = React.forwardRef<
     {...props}
   />
 ))
-Card.displayName = "Card"
+ShadCard.displayName = "shadCard"
 
 const CardHeader = React.forwardRef<
   HTMLDivElement,
@@ -76,4 +76,4 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { ShadCard, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
